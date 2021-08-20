029df3f862c1065dfcf25504ef3a6545794a1b1d
Walther <veeti.haapsamo@gmail.com>
2021-08-21 00:44:56 +0300

    Initial skeleton: comment enum, example files, etc

--- Walther/buttercup/src/lib.rs
+++ Walther/buttercup/src/lib.rs
@@ -1,7 +1,27 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#![deny(clippy::all)]
+
+#[derive(Debug, Clone)]
+pub struct Documentation {
+    content: String,
 }
+
+#[derive(Debug, Clone)]
+pub struct Assertion {
+    left: String,
+    right: String,
+}
+
+#[derive(Debug, Clone)]
+pub struct Trace {
+    content: String,
+}
+
+#[derive(Debug, Clone)]
+pub enum Comment {
+    Documentation(Documentation),
+    Assertion(Assertion),
+    Trace(Trace),
+}
+
+#[cfg(test)]
+mod tests {}
